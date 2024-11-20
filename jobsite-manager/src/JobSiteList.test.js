7c6171c552802b9ca505c6c47be78b409e6317de
kristok21 <kristokallfa@yahoo.com>
2024-11-20 01:05:17 +0100

    - Unit testing files added

---
+++ jobsite-manager/src/JobSiteList.test.js
@@ -0,0 +1,31 @@
+// JobSiteList.test.js
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import JobSiteList from "./components/JobSiteList/JobSiteList";
+import { BrowserRouter as Router } from "react-router-dom";
+
+describe("JobSiteList Component", () => {
+  test("renders JobSiteList component", () => {
+    render(
+      <Router>
+        <JobSiteList />
+      </Router>
+    );
+
+    expect(screen.getByText("Job Sites")).toBeInTheDocument();
+
+    expect(screen.getByText("Create")).toBeInTheDocument();
+  });
+
+  test("opens modal when Create button is clicked", () => {
+    render(
+      <Router>
+        <JobSiteList />
+      </Router>
+    );
+
+    fireEvent.click(screen.getByText("Create"));
+
+    expect(screen.getByText("Save")).toBeInTheDocument();
+  });
+});
